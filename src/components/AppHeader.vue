1c5c0e7b1e34eb301f9cc7106edb8982c644274b
Fesposito9197 <kekkoesp8@gmail.com>
2022-12-05 12:29:47 +0100

    aggiunge header e hero

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,53 @@
 <script>
+import AppHero from "./AppHero.vue";
 export default {
   name: "AppHeader",
+  components: {
+    AppHero,
+  },
 };
 </script>
 
 <template>
-  <div></div>
+  <header>
+    <div class="container d-flex justify-content-between">
+      <img src="../assets/white-logo-2.png" alt="" />
+      <div class="d-flex">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>Portafolio</li>
+        </ul>
+        <button class="my-btn my-btn-blue">Sign in</button>
+      </div>
+    </div>
+    <AppHero />
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: #0a164f;
+}
+img {
+  margin: 20px 0;
+}
+ul {
+  list-style: none;
+  color: white;
+  display: flex;
+  margin-top: 20px;
+  align-items: center;
+  li {
+    margin-left: 20px;
+  }
+}
+.my-btn {
+  margin-left: 40px;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "AppHero",
+};
+</script>
+
+<template>
+  <section class="d-flex container justify-content-between mt-3">
+    <div class="text-white business">
+      <h2 class="fw-bold fs-1">Business Planning</h2>
+      <h2 class="fw-light">Digital Technology</h2>
+      <h2 class="fw-bold fs-1">Modern Solution!</h2>
+      <p class="fw-light">
+        When, while the lovely valley teems with vapour around meand the
+        meridian sun strikes the upper surface.
+      </p>
+      <button class="my-btn my-btn-red my-btn-lg">Try it now</button>
+      <button class="my-btn my-btn-blue my-btn-lg">Explore More</button>
+    </div>
+    <div>
+      <img src="../assets/header.png" alt="" class="w-100" />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+p {
+  color: #047dab;
+  margin: 50px 0;
+  width: 400px;
+}
+.business {
+  margin: auto 0;
+}
+
+.my-btn:last-child {
+  margin-left: 20px;
+}
+</style>
