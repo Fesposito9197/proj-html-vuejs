ca022d9abaed5aa62d068bcf6877af4f346ad547
Fesposito9197 <kekkoesp8@gmail.com>
2022-12-06 16:21:43 +0100

    Aggiunge section info footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,15 @@
 <script>
+import AppInfoFooter from "./AppInfoFooter.vue";
 export default {
   name: "AppFooter",
+  components: {
+    AppInfoFooter,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <AppInfoFooter />
+</template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppInfoFooter.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  name: "AppInfoFooter",
+};
+</script>
+
+<template>
+  <section class="info-footer">
+    <div class="container d-flex">
+      <div>
+        <img src="../assets/white-logo-2.png" alt="" />
+        <p class="text-muted">
+          When, while the lovely valley teems with vapour around meand the
+          meridian sun strikes the upper surface.
+        </p>
+      </div>
+      <div>
+        <ul class="list-unstyled">
+          <p class="fw-bold text-white">Quick Links</p>
+          <li class="text-muted">Services</li>
+          <li class="text-muted">Contact</li>
+          <li class="text-muted">Blog</li>
+        </ul>
+      </div>
+      <div>
+        <ul class="list-unstyled">
+          <p class="fw-bold text-white">Resources</p>
+          <li class="text-muted">Art Design</li>
+          <li class="text-muted">Computer</li>
+          <li class="text-muted">IT News</li>
+          <li class="text-muted">Networkin</li>
+          <li class="text-muted">Web Security</li>
+        </ul>
+      </div>
+      <div>
+        <ul class="list-unstyled">
+          <p class="fw-bold text-white">Contact Us</p>
+          <li class="fw-bold text-white">
+            Address:
+            <span class="text-muted">
+              Patricia C.Amedee 4401 Waldeck Street Grapevine Nashville, Tx
+              76051</span
+            >
+          </li>
+          <li class="text-muted">Contact</li>
+          <li class="text-muted">Blog</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.info-footer {
+  background-color: #0a164f;
+}
+</style>
